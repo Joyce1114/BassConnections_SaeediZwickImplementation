7661057dfacc0de3025eda3fc16797ed5c95a3f9
Yue Xi <yx78@duke.edu>
2017-04-25 12:03:54 -0400

    add

--- evaluation.m
+++ evaluation.m
@@ -15,10 +15,12 @@ npixels = size1*size2;
 input2 = load('Norfolk_01_buildingCell_evaluation.mat');
 datafile = input2.building_cell;
 testdata = logical(zeros(size1,size2));
+% change testlabels to correct format
 for i = 1:length(datafile)-1
     shapeX = datafile{i+1,10};
     shapeY = datafile{i+1,11};
     counter = 0;
+    % remove the NAN values
     numNan=numel(find(isnan(shapeX)));
     numpoints=length(shapeX)-numNan;
     newX=zeros(numpoints,1);
@@ -35,11 +37,9 @@ for i = 1:length(datafile)-1
     BW = poly2mask(newX,newY,size1,size2);
     testdata = testdata | BW;
 end
+% change matrix to vector
 newtruelabels = testdata(:);
-% convert a matrix to array
 
-%reshape(matrix',npixels,1);
-%reshape(truelabels',npixels,1);
 % create a new object
 a = Result2(newmatrix,newtruelabels);
 % call the methods to calculate ROC curve and PR curve
